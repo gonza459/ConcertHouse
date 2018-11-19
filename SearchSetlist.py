d7b8f12034fd4b6ac0f814f614d4d2f026c8f86a
Samantha <samantha@Bernardos-iMac.local>
2018-11-19 15:10:36 -0800

    File name changes and Setify.py added

--- SearchSetlist.py
+++ SearchSetlist.py
@@ -9,26 +9,7 @@ import requests
 import os
 import json
 
-import PlaylistMaker
-
-#spotify API
-username = ""
-token = util.prompt_for_user_token(
-    username,
-    scope = 'playlist-modify-private playlist-modify-public',
-    #Enter Spotify API ID
-    client_id ='3817588cd345435c86c9a60e6c0cb70a',
-    #Enter Spotify API Secret
-    client_secret ='ce6c55f9f3c343bb919d917257661a3b',
-    #Enter Spotify API Redirect URI
-    redirect_uri='SpotifyTestApp://callback'
-)
-spotify = PlaylistMaker.createSpotifyToken(token)
-
-#setlist.fm API
-setCONSUMER_KEY = 'ecc895c8-fb79-4eab-be35-7d04744a15aa'
-
-headers = {'Accept': 'application/json', 'x-api-key': setCONSUMER_KEY}
+import SpotifyPlaylist
 
 #Get the MusicBrainz ID for the artist
 def getArtistID(header, name):
@@ -76,32 +57,3 @@ def getVenueSetlist(cityID, header):
                 return key
     else:
         return response
-
-print spotify
-print 'Please enter how you wish to search for looking up the setlist'
-print
-selection = raw_input('Enter 1 for artist name, 2 for date, 3 for venue: ')
-print
-
-#Input search term type (artist, date, or city/venue)
-if selection == '1':
-    name = raw_input("Enter artist name: ")
-    artistID = getArtistID(headers, name)
-    spotArtist = PlaylistMaker.searchTermsArtist(spotify, name)
-    print 'Setlist: '
-    print artistID
-    print getArtistSetlist(artistID, headers) #print data from setlist.fm
-    print
-    print 'Spotify: '
-    print spotArtist
-elif selection == '2':
-    print('Enter the date.')
-    month = raw_input('Enter the month: ')
-    day = raw_input('Enter the day: ')
-    year  = raw_input('Enter the year: ')
-elif selection == '3':
-    cityID = getCityID(headers)
-    print cityID
-    print getVenueSetlist(cityID, headers)
-else:
-    print('Not a valid input. Try again.')
